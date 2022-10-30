174d8abe6aeaccd4178448c62410b11c7a8fbd29
Stewart Gliniecki <stewgliniecki@gmail.com>
2022-10-30 16:53:32 +0000

    GH pages setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import search from './assets/search.png'
 import { displayInfomation } from './dom'
+import css from './styles.css'
 
 const searchIcon = document.querySelector('.searchIcon')
 searchIcon.src = search
